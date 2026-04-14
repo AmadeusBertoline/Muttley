60fe35f670e3f14191a6492d43a82c55dc0ad093
Amadeus Bertoline <amadeusbertoline123@gmail.com>
2026-04-14 19:24:07 -0300

    aluno

---
+++ src/main/java/com/example/Muttley/Aluno/Aluno.java
@@ -0,0 +1,32 @@
+package com.example.Muttley.Aluno;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "aluno")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of ="id")
+public class Aluno {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "aluno_id")
+	private long id;
+	private String nome;
+	private String curso;
+	private String email;
+
+}
